da9258ddea4320e2d235b8f03bfd79d7ad67890f
Linder Rodriguez <139185674+Yolian007@users.noreply.github.com>
2024-02-12 16:17:23 -0500

    Create mi_primera_app.py
    ready

---
+++ mi_primera_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Título y autor
+st.title("Mi primera app")
+st.text("Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE")
+
+# Preguntar nombre al usuario
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Imprimir mensaje de bienvenida
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
